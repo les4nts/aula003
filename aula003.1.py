327482316462dd9584711ff46ccd1715e125bb85
cleib <nycbducjd@gmail.com>
2025-03-31 15:48:24 -0300

    entrega do exercicio 003.2

---
+++ aula003.1.py
@@ -0,0 +1,12 @@
+number1 = input(float("digite o primeiro numero que deseja calcular"))
+number2 = input (float("digite o segundo numero que deseja calcular"))
+
+soma = number1+number2
+multiplicacao = number1*number2
+subtracao = number1-number2
+divisao = number1/number2
+
+print(f" a soma de {number1} + {number2} = {soma}")
+print(f"a multiplicação de {number1} * {number2} = {multiplicacao}")
+print(f"a subtração de {number1} - {number2} = {subtracao}")
+print(f"a divisão de {number1} / {number2} = {divisao}")
