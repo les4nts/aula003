c735547ede2315d6f9a6deb8fc9af709f017a021
cleib <nycbducjd@gmail.com>
2025-03-31 14:07:42 -0300

    envio do exercicio 003

---
+++ aula 003.py
@@ -0,0 +1,5 @@
+nome = input("qual o seu nome?")
+idade = int(input("qual a sua idade?"))
+salario = float(input("agora me informe o seu salario:"))
+
+print (f"olá, {nome} você atualmente tem {idade} e recebe cerca de {salario} , por mês. ")
