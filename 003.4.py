6e81e79cba1d56535368c545f86316f5a8f529d1
cleib <nycbducjd@gmail.com>
2025-03-31 16:52:29 -0300

    entrega das notas 2.0

--- 003.4.py
+++ 003.4.py
@@ -1,10 +1,14 @@
+from selectors import SelectSelector
+
 n1 = float(input(" nota 1:"))
 n2 = float(input(" nota 2:"))
 n3 = float(input(" nota 3:"))
 
-media = (n1+n2+n3)/3
-if media>=6.0:
-    print (f"aprovado. a sua media é {media}")
+media = (n1+n2+n3)//3
+if media>=7.0:
+    print (f"aprovado. a sua media é {media:.f2}")
 else:
-    if media<=6.0:
-        print(f"reprovado. sua media foi {media}")
+    if media<4.0:
+        print(f"reprovado. sua media foi {media:.f2}")
+    else:
+        print(f"recuperação. sua media foi {media:.f2}")
