08b6a2085a83a7d6161feb7b9fe0b79d6e03d3ea
cleib <nycbducjd@gmail.com>
2025-03-31 14:53:15 -0300

    entrega da complementação

--- aula 003.py
+++ aula 003.py
@@ -1,5 +1,8 @@
 nome = input("qual o seu nome?")
 idade = int(input("qual a sua idade?"))
 salario = float(input("agora me informe o seu salario:"))
-
-print (f"olá, {nome} você atualmente tem {idade} e recebe cerca de {salario} , por mês. ")
+porcentagem= float ( input("quanto voce recebeu de aumento em porcentagem ultimo ajuste:"))
+conta = (porcentagem*salario)/100
+somaaumento = (salario+conta)
+print (f"olá, {nome} você atualmente tem {idade} e recebia cerca de {salario} , por mês , antes do aumento. ")
+print (f" voce teve um aumento de {conta} no seu salario que era {salario}, então seu salário agora é {somaaumento}")
