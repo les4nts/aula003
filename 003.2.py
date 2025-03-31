cc198d8cd8746dd41dc036eea353b11e7d2e19f4
cleib <nycbducjd@gmail.com>
2025-03-31 16:00:46 -0300

    variavel

---
+++ 003.2.py
@@ -0,0 +1,10 @@
+a = input("digite o valor para a:")
+b = input("digite o valor para b:")
+print(f" valor de a, {a}")
+print(f"valor de b, {b}")
+x = a
+a = b
+b = x
+print(f"a: {a}")
+print(f"b: {b}")
+
